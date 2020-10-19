7417d7437133cf641ede5f0c96bee8062b37c459
chamin96 <chaminmax@gmail.com>
2020-10-19 19:35:47 +0530

    basic auth initialized

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -10,7 +10,7 @@ const firebaseConfig = {
     storageBucket: "demoapp-b6e14.appspot.com",
     messagingSenderId: "105052952144",
     appId: "1:105052952144:web:1b4efad7af8a2de3142355",
-    measurementId: "G-KFDW9X8TWB"
+    // measurementId: "G-KFDW9X8TWB"
 };
 
 if (!firebase.apps.length) {
